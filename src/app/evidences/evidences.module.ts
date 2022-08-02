ee798222b0d2e946bd45adf04321cb5ffb4b97b6
cavire <108669764+cavire@users.noreply.github.com>
2022-08-02 14:27:07 +0200

    Importar módulos y componentes
    Componente EvidenceUpload, módulos formulario PrimeNg, HttpClientModule.

--- src/app/evidences/evidences.module.ts
+++ src/app/evidences/evidences.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EvidenceListComponent } from './views/evidence-list/evidence-list.component';
-
-
+import { EvidenceUploadComponent } from './views/evidence-upload/evidence-upload.component';
+import { ButtonModule } from 'primeng/button';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { FileUploadModule } from 'primeng/fileupload';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    EvidenceListComponent
+    EvidenceListComponent,
+    EvidenceUploadComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonModule,
+    FileUploadModule,
+    DynamicDialogModule,
+    CheckboxModule,
+    FormsModule,
+    HttpClientModule
   ]
 })
 export class EvidencesModule { }
