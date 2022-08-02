4d8e60769c028232f798998e750e6079c9bdcebb
cavire <108669764+cavire@users.noreply.github.com>
2022-08-02 14:25:00 +0200

    Implementar módulo, crear botón para diálogo
    EvidenceList: crear botón para abrir diálogo EvidenceUpload

--- src/app/evidences/views/evidence-list/evidence-list.component.ts
+++ src/app/evidences/views/evidence-list/evidence-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { DialogService } from 'primeng/dynamicdialog';
+import { EvidenceUploadComponent } from '../evidence-upload/evidence-upload.component';
 
 @Component({
   selector: 'app-evidence-list',
   templateUrl: './evidence-list.component.html',
-  styleUrls: ['./evidence-list.component.scss']
+  styleUrls: ['./evidence-list.component.scss'],
+  providers: [DialogService]
 })
 export class EvidenceListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialogService: DialogService
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  importarDatos(): void {
+    const dialogRef = this.dialogService.open(EvidenceUploadComponent, {header: "Subir archivo"});
   }
 
 }
