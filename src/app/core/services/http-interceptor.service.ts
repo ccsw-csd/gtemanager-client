3fa9446e3a8dcb1e955d6284e2624e96f4ee925a
cavire <108669764+cavire@users.noreply.github.com>
2022-08-26 14:14:25 +0200

    Modificar HttpInterceptor para mostrar errores

--- src/app/core/services/http-interceptor.service.ts
+++ src/app/core/services/http-interceptor.service.ts
@@ -13,32 +13,39 @@ export class HttpInterceptorService implements HttpInterceptor {
   constructor(private auth: AuthService, private router: Router) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    
+
     let token: string | null = this.auth.getToken();
 
-    if(token != null) {
+    if (token != null) {
       const cloned = req.clone({
         headers: req.headers.set('Authorization', 'Bearer ' + token),
       });
       return next.handle(cloned).pipe(
-        catchError((error : HttpErrorResponse) => {
+        catchError((error: HttpErrorResponse) => {
           let errorMessage;
           switch (error.status) {
+            case 400:
+              errorMessage = 'Error de usuario: ' + error.error;
+              break;
             case 401:
               this.auth.logout();
               errorMessage = 'Token has expired';
               break;
             case 400:
             case 404:
-              this.router.navigateByUrl('/main');
-              errorMessage = 'Resource not found';
+              errorMessage = 'Recurso no encontrado.';
+              break;
+            case 415:
+              errorMessage = 'No se ha enviado un elemento válido.';
+              break;
+            case 422:
+              errorMessage = 'No se puede procesar el elemento enviado.';
               break;
             case 500:
-              this.router.navigateByUrl('/main');
-              errorMessage = 'Server-side error: ' + error.error.message;
+              errorMessage = 'Se ha producido un error del servidor. Por favor, póngase en contacto con un administrador. Disculpe las molestias.';
               break;
             default:
-              errorMessage = 'An error has ocurred';
+              errorMessage = 'Se ha producido un error. Por favor, inténtelo de nuevo.';
           }
           return throwError(() => new Error(errorMessage));
         })
