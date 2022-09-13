9e4e0ea8d0bb84c0f0c8e4b3354ce908d6219c46
cavire <108669764+cavire@users.noreply.github.com>
2022-09-13 16:14:23 +0200

    Comment: diseño y animación de carga

--- src/app/evidences/views/comment/comment.component.ts
+++ src/app/evidences/views/comment/comment.component.ts
@@ -15,36 +15,41 @@ export class CommentComponent implements OnInit {
 
   data: Comment;
   text: String;
+  isLoading: boolean;
 
   constructor(private evidenceService: EvidenceService,
     private commentService: CommentService,
     public ref: DynamicDialogRef,
     public config: DynamicDialogConfig,
     private snackbarService: SnackbarService,
-    ) { }
+  ) { }
 
   ngOnInit(): void {
-
     if (this.config.data.commentData != null) {
-      this.data = Object.assign({commentData: Comment}, this.config.data.commentData);
+      this.data = Object.assign({ commentData: Comment }, this.config.data.commentData);
     } else {
+      this.isLoading = true;
       this.data = new Comment();
-      this.evidenceService.getPersonById(this.config.data.id).subscribe( res => {
+      this.evidenceService.getPersonById(this.config.data.id).subscribe(res => {
         this.data.person = res;
+        this.isLoading = false;
       });
       this.data.comment = "";
     }
   }
 
   editComment(item: Comment) {
-    if (this.data.comment != "") {
+    if (this.data.comment) {
+      this.isLoading = true;
       this.commentService.saveComment(item).subscribe({
         next: () => {
           this.snackbarService.showMessage("El registro se ha guardado con éxito");
+          this.isLoading = false;
           this.onClose();
         },
         error: () => {
           this.snackbarService.error("El registro no se ha podido guardar correctamente");
+          this.isLoading = false;
         }
       });
     }
