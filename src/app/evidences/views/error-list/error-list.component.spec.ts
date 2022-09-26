cbc4c4e253055571fb46cf496b837d6ab7382ea7
Cristal <camacri2@alumni.uv.es>
2022-09-26 16:23:59 +0200

    Testing

--- src/app/evidences/views/error-list/error-list.component.spec.ts
+++ src/app/evidences/views/error-list/error-list.component.spec.ts
@@ -1,25 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { of, throwError } from 'rxjs';
 import { ErrorListComponent } from './error-list.component';
 
 describe('ErrorListComponent', () => {
-  let component: ErrorListComponent;
-  let fixture: ComponentFixture<ErrorListComponent>;
+  let errorListComponent: ErrorListComponent;
+  let mockErrorService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ErrorListComponent ]
-    })
-    .compileComponents();
-  });
+  let DATA_LIST = [
+    {id:1, name:null, saga:null, email:null, period:null, message:null},
+    {id:2, name:null, saga:null, email:null, period:null, message:null},
+    {id:3, name:null, saga:null, email:null, period:null, message:null}
+  ]
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ErrorListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    mockErrorService = jasmine.createSpyObj(["findAll"]);
+    errorListComponent = new ErrorListComponent(mockErrorService);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('findAllShouldReturnErrors', () => {
+    mockErrorService.findAll.and.returnValue(of(DATA_LIST));
+    
+    errorListComponent.findAll();
+
+    expect(errorListComponent.errors).toEqual(DATA_LIST);
   });
 });
