2ca4d5679a18cc1f29251cea4adda8809fd76ecf
josep <jestevec@capgemini.com>
2022-10-21 12:02:53 +0200

    Añadido indicador de la notificación por email

--- src/app/evidences/model/Evidence.ts
+++ src/app/evidences/model/Evidence.ts
@@ -12,6 +12,7 @@ export class Evidence {
     evidenceTypeW4: EvidenceType;
     evidenceTypeW5: EvidenceType;
     evidenceTypeW6: EvidenceType;
+    emailNotificationSent: boolean;
 
     public constructor(init?:Partial<Evidence>) {
         Object.assign(this, init);
